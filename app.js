1ffe3727ce3b0ad15e918e1d2399e23d7516f04e
Dhivijit Koppuravuri <68179868+dhivijit@users.noreply.github.com>
2024-01-05 00:46:24 +0000

    Add nodemon as a dependency and update login form

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const path = require('path')
+// import the test function from the cb js file
+const cb = require('./cb')
+const crypto = require('crypto')
+
+function calculateSHA256Hash(inputString) {
+  // Create a SHA-256 hash object
+  const hash = crypto.createHash('sha256');
+
+  // Update the hash object with the input string
+  hash.update(inputString);
+
+  // Get the hexadecimal representation of the hash
+  const hashedString = hash.digest('hex');
+
+  return hashedString;
+}
+
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
 const port = process.env.port | 3000
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/main.html')
+  res.sendFile(__dirname + '/main.html')
+  cb.test()
 })
 
-app.post("/", (req, res) => {
-    const { name, email, password } = req.body
-    res.send(`Name: ${name} Email: ${email} Password: ${password}`)
+app.post("/login", (req, res) => {
+  const { username, password } = req.body
+  res.send(`Username: ${username} Password: ${password}`)
+  cred = username + "!|,$@(~()%?" + password
+  credhashed = calculateSHA256Hash(cred)
+  console.log(credhashed)
 })
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+  console.log(`Server running on port ${port}`)
 })
